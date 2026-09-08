4e319fd88e4325ffa6aac3881dc000408928b1af
joy1404 <joyrakshit12345@gmail.com>
2026-09-08 20:12:45 +0530

    feat: implement segment tree construction and range sum query functionality

--- Segment Tree/range_query_segmentTree.cpp
+++ Segment Tree/range_query_segmentTree.cpp
@@ -10,6 +10,16 @@ void buildTree(int i, int l, int r, int arr[], int segment[]){
     buildTree(2*i + 2, mid + 1, r, arr, segment);
     segment[i] = segment[2*i + 1] + segment[2*i + 2];
 }
+int rangeQuery(int i, int l, int r, int start, int end, int arr[], int segment[]){
+    if(end<l || start>r){
+        return 0;
+    }
+    if(l>=start && r<=end){
+        return segment[i];
+    }
+    int mid = (l+r)/2;
+    return rangeQuery(2*i + 1, l, mid, start, end, arr, segment) + rangeQuery(2*i + 2, mid + 1, r, start, end, arr, segment);
+}
 int main(){
     int n;
     cin>>n;
@@ -21,5 +31,7 @@ int main(){
     for(int i=0; i<4*n; i++){
         cout<<segment[i]<<" ";
     }
+    cout<<endl;
+    cout<<rangeQuery(0, 0, n-1, 1, 3, arr, segment)<<endl;
     return 0;
 }
